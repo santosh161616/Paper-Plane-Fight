cc4110c87ef2e487852b26dbfbfaa7500fc22582
Santosh Kumar <santosh161616@gmail.com>
2024-03-13 23:20:38 +0530

    Shop UI done and next/prev functions added

---
+++ Assets/Scripts/Shop.cs
@@ -0,0 +1,67 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Shop : MonoBehaviour
+{
+    [SerializeField] private Image _playerImage;
+    [SerializeField] private TMP_Text _playerName;
+    [SerializeField] private TMP_Text _health;
+    [SerializeField] private TMP_Text _damage;
+    [SerializeField] private TMP_Text _speed;
+
+    [SerializeField] private Button _nextButton, _prevButton;
+
+    [SerializeField] private PlayerSelectionData _selectedPlayer;
+    [SerializeField] private int defaultIndex = 0;
+
+    void Start()
+    {
+        SelectPlayer(_selectedPlayer, defaultIndex);
+        AddingListeners();
+    }
+    private void SelectPlayer(PlayerSelectionData selectionData, int index)
+    {
+        _playerImage.sprite = selectionData.playersData[index].playerImage;
+        _playerName.text = selectionData.playersData[index].PlayerName;
+        _health.text = selectionData.playersData[index].Health;
+        _damage.text = selectionData.playersData[index].Damage;
+        _speed.text = selectionData.playersData[index].Speed;
+    }
+
+    public void NextSelect()
+    {
+        if (defaultIndex != _selectedPlayer.playersData.Count)
+        {
+            SelectPlayer(_selectedPlayer, defaultIndex);
+            defaultIndex++;
+        }
+        else
+        {
+            defaultIndex = 0;
+            SelectPlayer(_selectedPlayer, defaultIndex);
+        }
+    }
+
+    public void PrevSelect()
+    {
+        if (defaultIndex < 0)
+        {
+            SelectPlayer(_selectedPlayer, defaultIndex);
+            defaultIndex--;
+        }
+        else
+        {
+            defaultIndex = _selectedPlayer.playersData.Count - 1;
+            SelectPlayer(_selectedPlayer, defaultIndex);
+        }
+    }
+
+    void AddingListeners()
+    {
+        _nextButton.onClick.RemoveAllListeners();
+        _nextButton.onClick.AddListener(() => { NextSelect(); });
+        _prevButton.onClick.RemoveAllListeners();
+        _prevButton.onClick.AddListener(() => { PrevSelect(); });
+    }
+}
